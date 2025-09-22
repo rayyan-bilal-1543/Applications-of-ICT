568e9d0a30df81a73de0ab1f8317b094c93a03f8
khayyat786 <mnhayyat786@gmail.com>
2025-09-22 09:27:05 -0700

    Add files via upload

---
+++ KHIZER HAYYAT/name age.py
@@ -0,0 +1,19 @@
+name = "KHIZER HAYYAT"
+age = 19
+height = 5.6
+is_student=True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+name = str(input("enter your name :"))
+age = int (input("enter your age :"))
+height = float(input("enter your height :"))
+is_student=bool(input("enter your is_student"))
+
+print("YOUR NAME IS : ",name)
+print("YOUR AGE IS : ",age)
+print("YOUR HEIGHT IS : ",height)
+print(is_student)
---
+++ KHIZER HAYYAT/operators.py
@@ -0,0 +1,21 @@
+# ARITHEMATIC OPERATORS
+a=2
+b=10
+
+print("add  ",a+b)
+print("subtract  ",a-b)
+print("multiply  ",a*b)
+print("divide  ",a/b)
+print("floor divide  ",a//b)
+print("modulus  ",a%b)
+print("exponent  ",a**b)
+
+# Relational operators
+x= a==10
+y= b==10
+z= a<10
+d= a>b
+e= a>=b
+f= a<=b
+g= a!=b
+print(x,y,z,d,e,f)
