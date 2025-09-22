740bab15444016ff665437fbea33ce2cf1ddfbd1
rayyan-bilal-1543 <rayyanbilal23@gmail.com>
2025-09-22 21:18:11 +0500

    Add files via upload

---
+++ Applications-of-ICT/Muhammad Rayyan Bilal/task1_variables.py
@@ -0,0 +1,20 @@
+name = "Muhammad Rayyan Bilal"
+age = 18
+height = 5.8
+is_student = True
+
+print (name)
+print (age)
+print (height)
+print (is_student)
+
+name = str(input("Enter your Name:"))
+age = int(input("Enter your Age:"))
+height = float(input("Enter your Height:"))
+is_student = bool(input("Enter your is_student:")) 
+
+
+print (name)
+print (age)
+print (height)
+print (is_student)
---
+++ Applications-of-ICT/Muhammad Rayyan Bilal/task2_arithmetic.py
@@ -0,0 +1,11 @@
+#arithmetic operations
+a = 2
+b = 10
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor division:", a//b)
+print("Modulus:", a%b)
+print("Exponent:", a**b)
